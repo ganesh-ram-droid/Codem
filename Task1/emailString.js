23c6828a206601cc772dbc78355edd450ce0f64c
ganesh-ram-droid <2315006@nec.edu.in>
2026-02-11 20:55:13 +0530

    2315006-Task1 Completed

--- Task1/emailString.js
+++ Task1/emailString.js
@@ -1,8 +1,8 @@
 
-let email = "mganeshram@gmail.com"
+let email = "2315006@nec.edu.in"
 //extracting username
-let userName  =email.substring(0,email.indexOf("@"))
+let userName  =email.split("@")[0]
 //extracting domain
-let domain = email.substring(email.indexOf("@")+1)
+let domain = email.split("@")[1].split(".")[0]
 console.log(`UserName is ${userName} and domain name is ${domain}`);
 
--- Task1/simpleIntrest.js
+++ Task1/simpleIntrest.js
@@ -1,6 +1,10 @@
 //creating variables
 let principalAmount = 10000
+
 let rateOfIntrest = 5
+
 let timePeriod = 3
+
 let result = (principalAmount*rateOfIntrest*timePeriod)/100
+
 console.log(`The Simple Intrest is:${result}`)
